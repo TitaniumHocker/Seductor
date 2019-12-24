ca5511fae8437efd8e101c89a38fa7c346492ea6
Titanium Hocker <inbox@titaniumhocker.ru>
2019-12-24 08:54:02 +0000

    finished mainpage template and styling

--- seductor/views.py
+++ seductor/views.py
@@ -7,54 +7,20 @@ import base62
 
 
 @app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        url = request.form.get('original_url')
-        query = Links.query.filter_by(original_url=url).first()
-        if query != None:
-            short_url = f'http://{app.config["DOMAINS"][-1]}/{base62.encode(query.id)}'
-            return f'<h1>{short_url}</h1>'
-
-        link = Links(original_url=url)
-        db.session.add(link)
-        db.session.commit()
-        db.session.refresh(link)
-        short_url = f'http://{app.config["DOMAINS"][-1]}/{base62.encode(link.id)}'
-        return f'<h1>{short_url}</h1>'
-
-    return '''
-    <form action="/" method="POST">
-        <input type="url" name="original_url" placeholder="type your url here">
-        <button type="submit">allah</button>
-    </form>
-    '''
+def index_page():
+    return render_template('index.html')
 
 
-@app.route('/top', methods=['GET'])
-def top_page():
-    return 'top page'
-
-
-@app.route('/stats', methods=['GET', 'POST'])
-def stats_page():
-    return 'stats page'
-
-
-@app.route('/about', methods=['GET'])
+@app.route('/about', methods=['GET', 'POST'])
 def about_page():
-    return 'about page'
+    return render_template('about.html')
 
 
-@app.route('/<magical_url>')
-def magic(magical_url):
-   link = Links.query.filter_by(id=base62.decode(magical_url)).first_or_404()
-   link.visits += 1
-   db.session.commit()
-   return redirect(link.original_url)
+@app.route('/top', methods=['GET', 'POST'])
+def top_page():
+    return render_template('top.html')
 
 
 @app.errorhandler(404)
 def not_found_page(e):
-    return '<h1>Not Found</h1>', 404
-
-
+    return render_template('404.html'), 404

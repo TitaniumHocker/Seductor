e71b0f274e2e6e6db2b3e6aa6f004052ac45f251
Titanium Hocker <inbox@titaniumhocker.ru>
2020-01-28 21:01:59 +0000

    add some routes and methods for api

--- seductor/api/views.py
+++ seductor/api/views.py
@@ -6,4 +6,23 @@ from ..models import *
 
 @api.route('/')
 def index():
-    return 'test api index page'
+    return 'You can find API documentation on ...'
+
+@api.route('/link', methods=['GET', 'POST'])
+def get_link():
+    pass
+
+@api.route('/top', methods=['GET'])
+def get_top():
+    pass
+
+@api.route('/tkn<token>/link', methods=['GET', 'POST', 'DELETE', 'PUT'])
+def auth_link(token):
+    pass
+
+# Необязательные параметры: id, время начала, время конца, 
+@api.route('/tkn<token>/stat', methods=['POST'])
+def auth_stats(token):
+    pass
+
+
